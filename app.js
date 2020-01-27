20ad864d7f25b59cb9332250a265bd97dec0a3ab
Cristian Ruiz <cristianruiz@MacBook-Pro.local>
2020-01-27 11:35:43 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//REQUIRES 
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+//Inicializar variables
+
+var app = express();
+
+//Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) => {
+
+    if (err) throw err;
+    console.log('Base de datos: \x1b[45m', 'online')
+});
+
+//Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
+
+//Escuchar peticiones
+
+app.listen(3000, () => {
+    console.log('Express Server corriendo en el puerto 3000 : \x1b[45m', 'online')
+});
